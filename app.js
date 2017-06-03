52b4ff978a8ac2993111907d31c612285768809b
mynlop <mynlop15@gmail.com>
2017-06-02 19:32:39 -0600

    se instalo express, y que agrego el hola mundo

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("hola mundo");
+const express = require('express')
+
+const app = express()
+
+app.get('/', function(req, res){
+    res.send('Hola mundo')
+})
+
+app.listen(3000, function(){
+    console.log("Servidor corriendo en el puerto 3000...")
+})
