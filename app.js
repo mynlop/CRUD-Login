0eac5d86a1299f85b5b3facd523c8f0c6b88e4d3
mynlop <mynlop15@gmail.com>
2017-06-03 07:28:53 -0600

    hola mundo con pug

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express')
+const path = require('path')
 
+//iniciando la app
 const app = express()
 
+//asignando la vista
+app.set('views', path.join(__dirname,'views'))
+app.set('view engine','pug')
+
+//ruta a la pagina raiz
 app.get('/', function(req, res){
-    res.send('Hola mundo')
+    res.render('index')
 })
 
+//iniciando server
 app.listen(3000, function(){
     console.log("Servidor corriendo en el puerto 3000...")
 })
