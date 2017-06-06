b6f5744e12a60fe1bce03506bcf31d5cc3bd0f18
mynlop <mynlop15@gmail.com>
2017-06-05 21:20:40 -0600

    se agrego el uso de router, para tener que el archivo app fuera mas legible, y por los mismo se corrigieron rutas en los diferentes archivos

--- app.js
+++ app.js
@@ -85,95 +85,9 @@ app.get('/', function(req, res){
     })
 })
 
-//obtener id del articulo
-app.get('/article/:id', function(req, res){
-    Article.findById(req.params.id, function(err, article){
-        res.render('article', {
-            article: article
-        })
-    })
-})
-
-//generar formulario de edicion de x articulo
-app.get('/article/edit/:id', function(req, res){
-    Article.findById(req.params.id, function(err, article){
-        res.render('editArticle', {
-            title: 'Editar Artiulo',
-            article: article
-        })
-    })
-})
-
-//agregar una ruta
-app.get('/articles/add',function(req,res){
-    res.render('addArticle',{
-        title: 'Agregar articulo'
-    })
-})
-
-//ruta de post en guardar articulo
-app.post('/articles/add',function(req,res){
-    req.checkBody('title','El tiulo es obligatorio.').notEmpty()
-    req.checkBody('author','El autor es obligatorio.').notEmpty()
-    req.checkBody('body','El cuerpo es obligatorio.').notEmpty()
-
-    // get errors
-    let errors = req.validationErrors()
-
-    if(errors){
-        res.render('addArticle', {
-            title: 'Agregar Articulo',
-            errors: errors
-        })
-    }else{
-        let article = Article()
-        article.title = req.body.title
-        article.author = req.body.author
-        article.body = req.body.body
-
-        article.save(function(err){
-            if(err){
-                console.log(err)
-                return
-            }else{
-                req.flash('success','Articulo agregado.')
-                res.redirect('/')
-            }
-
-        })
-    }
-})
-
-//ruta de update los cambios en x articulo
-app.post('/article/edit/:id',function(req,res){
-    let article = {}
-    article.title = req.body.title
-    article.author = req.body.author
-    article.body = req.body.body
-
-    let query = {_id: req.params.id}
-    Article.update(query, article, function(err){
-        if(err){
-            console.log(err)
-            return
-        }else{
-            req.flash('success','Cambios realizados en el articulo.')
-            res.redirect('/')
-        }
-
-    })
-})
-
-// eliminar un articulo
-app.delete('/article/:id', function(req, res){
-    let query = {_id: req.params.id}
-    Article.remove(query, function(err){
-        if(err){
-            console.log(err)
-        }
-        res.send('Success')
-    })
-})
+//ruta a archivos
+let articles = require('./routes/articles')
+app.use('/articles',articles)
 
 //iniciando server
 app.listen(3000, function(){
---
+++ routes/articles.js
@@ -0,0 +1,97 @@
+const express = require('express')
+const router = express.Router()
+
+//traer el modelo Article
+let Article = require('../models/articles')
+
+//generar formulario de edicion de x articulo
+router.get('/edit/:id', function(req, res){
+    Article.findById(req.params.id, function(err, article){
+        res.render('editArticle', {
+            title: 'Editar Artiulo',
+            article: article
+        })
+    })
+})
+
+//agregar una ruta
+router.get('/add',function(req,res){
+    res.render('addArticle',{
+        title: 'Agregar articulo'
+    })
+})
+
+//ruta de post en guardar articulo
+router.post('/add',function(req,res){
+    req.checkBody('title','El tiulo es obligatorio.').notEmpty()
+    req.checkBody('author','El autor es obligatorio.').notEmpty()
+    req.checkBody('body','El cuerpo es obligatorio.').notEmpty()
+
+    // get errors
+    let errors = req.validationErrors()
+
+    if(errors){
+        res.render('addArticle', {
+            title: 'Agregar Articulo',
+            errors: errors
+        })
+    }else{
+        let article = Article()
+        article.title = req.body.title
+        article.author = req.body.author
+        article.body = req.body.body
+
+        article.save(function(err){
+            if(err){
+                console.log(err)
+                return
+            }else{
+                req.flash('success','Articulo agregado.')
+                res.redirect('/')
+            }
+
+        })
+    }
+})
+
+//ruta de update los cambios en x articulo
+router.post('/edit/:id',function(req,res){
+    let article = {}
+    article.title = req.body.title
+    article.author = req.body.author
+    article.body = req.body.body
+
+    let query = {_id: req.params.id}
+    Article.update(query, article, function(err){
+        if(err){
+            console.log(err)
+            return
+        }else{
+            req.flash('success','Cambios realizados en el articulo.')
+            res.redirect('/')
+        }
+
+    })
+})
+
+// eliminar un articulo
+router.delete('/:id', function(req, res){
+    let query = {_id: req.params.id}
+    Article.remove(query, function(err){
+        if(err){
+            console.log(err)
+        }
+        res.send('Success')
+    })
+})
+
+//obtener id del articulo
+router.get('/:id', function(req, res){
+    Article.findById(req.params.id, function(err, article){
+        res.render('article', {
+            article: article
+        })
+    })
+})
+
+module.exports = router
