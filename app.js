3d669de4cd16cb00b783dd078f836d4500e698c3
mynlop <mynlop15@gmail.com>
2017-06-05 20:59:26 -0600

    se agregaron librerias, y se trabajo en validacion y mostrar mensajes personalizados

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const path = require('path')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const expressValidator = require('express-validator')
+const flash = require('connect-flash')
+const session = require('express-session')
 
 mongoose.connect('mongodb://localhost/mongoBase')
 let db = mongoose.connection
@@ -35,6 +38,38 @@ app.use(bodyParser.json())
 
 //asignar la carpeta publica
 app.use(express.static(path.join(__dirname,'public')))
+
+// expres session 
+app.use(session({
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized: false
+}))
+
+//express messages
+app.use(require('connect-flash')())
+app.use(function(req,res,next){
+    res.locals.messages = require('express-messages')(req, res)
+    next()
+})
+
+//express validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
 
 //ruta a la pagina raiz
 app.get('/', function(req, res){
@@ -78,20 +113,35 @@ app.get('/articles/add',function(req,res){
 
 //ruta de post en guardar articulo
 app.post('/articles/add',function(req,res){
-    let article = Article()
-    article.title = req.body.title
-    article.author = req.body.author
-    article.body = req.body.body
+    req.checkBody('title','El tiulo es obligatorio.').notEmpty()
+    req.checkBody('author','El autor es obligatorio.').notEmpty()
+    req.checkBody('body','El cuerpo es obligatorio.').notEmpty()
 
-    article.save(function(err){
-        if(err){
-            console.log(err)
-            return
-        }else{
-            res.redirect('/')
-        }
+    // get errors
+    let errors = req.validationErrors()
 
-    })
+    if(errors){
+        res.render('addArticle', {
+            title: 'Agregar Articulo',
+            errors: errors
+        })
+    }else{
+        let article = Article()
+        article.title = req.body.title
+        article.author = req.body.author
+        article.body = req.body.body
+
+        article.save(function(err){
+            if(err){
+                console.log(err)
+                return
+            }else{
+                req.flash('success','Articulo agregado.')
+                res.redirect('/')
+            }
+
+        })
+    }
 })
 
 //ruta de update los cambios en x articulo
@@ -107,6 +157,7 @@ app.post('/article/edit/:id',function(req,res){
             console.log(err)
             return
         }else{
+            req.flash('success','Cambios realizados en el articulo.')
             res.redirect('/')
         }
 
